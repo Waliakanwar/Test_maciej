3bc0ebf9cb8e2f12b1fe564b0c43de5b64f6c3c4
maciejc <maciejc@educ301-43.cbm.local>
2019-07-09 13:28:02 +0200

    ver 4

--- Test_Git/src/Main.java
+++ Test_Git/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("test version 1.2 von  Maciej");
 		System.out.println("test version 1.3 von  Maciej");
+		System.out.println("test version 1.4 von  Maciej");
 		System.out.println("test version 1 von Kanvar");
 
 	}
