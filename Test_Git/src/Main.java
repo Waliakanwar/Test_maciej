4544e076436c81452fa98fe1105b5d7c2933db77
Kanwarpals <Kanwarpals@educ301-44.cbm.local>
2019-07-09 13:15:17 +0200

    version 3

--- Test_Git/src/Main.java
+++ Test_Git/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test version 1 von Kanvar");
+		System.out.println("test version 1.3 von Kanvar");
 
 	}
 
